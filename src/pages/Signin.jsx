d786462716b0c76195b803ecc230ff3f114c605c
devranjit <dvranjitplays@gmail.com>
2024-12-12 00:46:19 +0600

    register setup

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -9,8 +9,12 @@ const Signin = () => {
     };
 
     return (
+        <>
+        
         <div className="mx-auto grid grid-cols-1 lg:flex md:flex sm:flex  justify-center py-52 bg-gray-700">
+            
             <div className=" border-2 border-gray-500 rounded-lg p-32  w-[40%]">
+                <h1 className='text-center text-2xl py-2 text-white'>Sign In </h1>
                 <div className="border-2 rounded-full">
                     <input className="focus:outline-none w-full py-2 px-4 rounded-full" type="text" placeholder="Your name" />
                 </div>
@@ -25,6 +29,11 @@ const Signin = () => {
                 <Lottie options={animationOptions} height={400} width={700} />
             </div>
         </div>
+        
+        
+        
+        
+        </>
     );
 };
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,45 @@
-
+import Lottie from 'react-lottie';
+import loginAnimation from '../assets/animaiton.json';  
 
 const Signup = () => {
+    const animationOptions = {
+        loop: true, 
+        autoplay: true,
+        animationData: loginAnimation, 
+    };
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+
+
+    }
+
     return (
-        <div>
-            <h1>signup</h1>
+        <div >
+            <form onSubmit={handleRegister} className="mx-auto grid grid-cols-1 lg:flex md:flex sm:flex  justify-center py-52 bg-gray-700" >
+            <div className=" border-2 border-gray-500 rounded-lg p-32  w-[40%]">
+                <h1 className='text-center text-2xl py-2 text-white'>Sign up</h1>
+                <div className="border-2 my-2 rounded-full">
+                    <input name='email' className="focus:outline-none w-full py-2 px-4 rounded-full" type="email" placeholder="Your Email" />
+                </div>
+                <div className="border-2  rounded-full">
+                    <input  name='password' className="focus:outline-none w-full py-2 px-4 rounded-full" type="text" placeholder="Your Password" />
+                    
+                </div>
+                <p className='text-white my-0 py-2'>Forget password?</p>
+                <div>
+                    <button className='w-full bg-blue-400 text-white border border-black  text-center py-2 rounded-full text-xl font-bold uppercase' type='submit'>Sign Up</button>
+                </div>
+            </div>
+            <div>
+                <Lottie options={animationOptions} height={400} width={700} />
+            </div>
+            </form>
+           
         </div>
     );
 };
